60007bf169a3ab2ee250bbda3492847798f00add
Anirban Dey <anirbandeytech@gmail.com>
2026-08-22 21:18:01 +0530

    Use href instead of routerLink
    Otherwise links were encoding # as %23

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
@@ -11,10 +10,10 @@ export class HeaderComponent {
   public menuOpen = signal(false);
 
   public items: { title: string; href: string }[] = [
-    { title: 'About', href: '/#about' },
-    { title: 'Skills', href: '/#skills' },
+    // { title: 'About', href: '/#about' },
+    // { title: 'Skills', href: '/#skills' },
     { title: 'Experience', href: '/#experience' },
-    { title: 'Certification', href: '/#certification' },
+    { title: 'Certification', href: '/#certifications' },
     { title: 'Education', href: '/#education' },
     { title: 'Contact', href: '/#contact' },
   ];
