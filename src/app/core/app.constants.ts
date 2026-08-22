0dc4e757f6a5a62bcc0859d6ff10965af54f5f18
Anirban Dey <anirbandeytech@gmail.com>
2026-08-22 20:44:38 +0530

    Add urls in app.constants.ts

---
+++ src/app/core/app.constants.ts
@@ -0,0 +1,3 @@
+export const CV_URL = 'https://drive.google.com/file/d/1ODIqArlA__jes9wHuR79pG2ShZo74OIG/view?usp=sharing&usp=embed_facebook'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/anirbandeytech'
+export const CONTACT_EMAIL = 'anirbandeytech@gmail.com'
