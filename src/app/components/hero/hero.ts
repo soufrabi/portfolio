97a3b4e047b6eb2be630628a979097f07d0caf00
Anirban Dey <anirbandeytech@gmail.com>
2026-08-22 20:45:30 +0530

    Get cvUrl from app.constants in hero component

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CV_URL } from '../../core/app.constants';
 
 @Component({
   selector: 'app-hero',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  public readonly cvUrl = CV_URL;
+}
