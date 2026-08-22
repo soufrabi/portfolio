facf4f134dbd35a4e358d4775c0db27410617620
Anirban Dey <anirbandeytech@gmail.com>
2026-08-22 11:44:36 +0530

    Add certifications component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { Hero } from "./components/hero/hero";
+import { Certifications } from "./components/certifications/certifications";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Hero],
+  imports: [RouterOutlet, Header, Footer, Hero, Certifications],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/certifications/certifications.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+interface Certification {
+  title: string;
+  issuer: string;
+  icon: string;
+  url: string;
+}
+
+@Component({
+  selector: 'app-certifications',
+  imports: [],
+  templateUrl: './certifications.html',
+  styleUrl: './certifications.scss',
+})
+export class Certifications {
+    certifications: Certification[] = [
+    {
+      title: 'Microsoft Certified: Azure Fundamentals',
+      issuer: 'Microsoft',
+      icon: '🏅',
+      url: 'https://learn.microsoft.com/api/credentials/share/en-us/AnirbanDey-7718/6AEF5E7103BDEBC8?sharingId=5DCE6DAF916F43F4'
+    },
+    {
+      title: 'Microsoft Certified: Azure AI Fundamentals',
+      issuer: 'Microsoft',
+      icon: '📘',
+      url: 'https://learn.microsoft.com/api/credentials/share/en-us/AnirbanDey-7718/C8D368B33F6CAD44?sharingId=5DCE6DAF916F43F4'
+    },
+     {
+      title: 'AWS Certified Cloud Practitioner',
+      issuer: 'Amazon Web Services',
+      icon: '☁️',
+      url: 'https://www.credly.com/badges/bf4a2577-743b-4940-b879-8eafa165d22e/public_url'
+    },
+     {
+      title: 'HashiCorp Certified: Terraform Associate',
+      issuer: 'HashiCorp',
+      icon: '🏗️',
+      url: 'https://www.credly.com/badges/301b6bc5-f4c1-4a3d-94eb-9bc6f4036252/public_url'
+    }
+  ];
+
+  openCertification(url: string): void {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+}
